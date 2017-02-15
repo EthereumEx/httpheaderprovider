1403023b36ee55d79bfb8e9d1e7579e9d4a5b775
cicorias <github@cicoria.com>
2017-02-15 14:08:36 -0500

    updates for tests

--- test/test-connect-remote-header.js
+++ test/test-connect-remote-header.js
@@ -1,29 +1,91 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+var chalk = require('chalk');
 var Web3 = require('web3');
 var web3 = new Web3();
 
-var HttpHeaderProvider = require('../lib');
 
+var parms = JSON.parse( fs.readFileSync(__dirname + '/../parms.json') );
+
+
+/* This is the ony change - these 3 chunks */
+//chunk 1
+var HttpHeaderProvider = require('../lib');
+//chunk 2
 var headers = {
-  "Ocp-Apim-Subscription-Key": "<apikeyhere>"
+  "Ocp-Apim-Subscription-Key": parms.apikey
 }
 
-var url = "https://scicoria.azure-api.net/geth/";
+var httpProvider = new Web3.providers.HttpProvider(parms.apiurl);
 
-console.log("talking to remote Geth on %s", url)
-var provider = new HttpHeaderProvider(url, headers);
+//chunk 3
+var newProvider = new HttpHeaderProvider(parms.apiurl, headers);
 
-web3.setProvider(provider);
+function makeCall(provider) {
+  web3.setProvider(provider);
+  console.log(chalk.green.underline.bold("talking to remote Geth on %s"), parms.apiurl);
+  console.log(chalk.cyan.bold("ethereum client info: %s"), web3.version.node);
+  console.log(chalk.yellow.bold("Peer Count: %d"), web3.net.peerCount);
+
+  var coinbase = web3.eth.coinbase;
+  console.log(chalk.cyan.bold("web3.eth.coinbase: %s"), coinbase);
+
+  //replace the hex with a valid account addr
+  var balance = web3.eth.getBalance(parms.address);
+
+  console.log(
+    chalk.yellow.bold("web3.eth.getBalance(addr)\n\t"), 
+    chalk.green(balance.toString(10)), 
+    chalk.blue('wei\n\t'),
+    chalk.blue.bold(web3.fromWei(balance, 'ether')), 
+    chalk.green('ether'));
+}
 
 
-console.log("ethereum client info: %s", web3.version.node);
-console.log("Peer Count: %d", web3.net.peerCount);
+// First call is via base httpprovider - this will FAIL
+try {
+  console.log(chalk.green.bold.underline('Call via normal provider'))
+  makeCall(httpProvider);
+} catch (error) {
+  console.error(chalk.red.bold.underline("\tfail on httpprovider call: "))
+  console.error(chalk.red.bold('\t\t%s'), error.message);
+}
 
-var coinbase = web3.eth.coinbase;
-console.log("web3.eth.coinbase: %s", coinbase);
+// second call is via custom extended httprovider - this should succeed
+try {
+  console.log(chalk.green.bold.underline('\n\nCall via custom provider'))
+  makeCall(newProvider);
+} catch (error) {
+  console.error(chalk.red.bold.underline("\t\tfail on new provider call: "))
+  console.error(chalk.red.bold('\t\t%s'), error.message);
+}
 
-//replace the hex with a valid account addr
-var balance = web3.eth.getBalance('13015840b5b4641f3ad441e36ec428d7a1c9934c');
-console.log("web3.eth.getBalance(addr)", balance.toString(10));
 
+
+
+/*
+WEI	1
+Ada	1000
+Fentoether	1000
+Kwei	1000
+Mwei	1000000
+Babbage	1000000
+Pictoether	1000000
+Shannon	1000000000
+Gwei	1000000000
+Nano	1000000000
+Szabo	1000000000000
+Micro	1000000000000
+Microether	1000000000000
+Finney	1000000000000000
+Milli	1000000000000000
+Milliether	1000000000000000
+Ether	1000000000000000000
+Einstein	1000000000000000000000
+Kether	1000000000000000000000
+Grand	1000000000000000000000
+Mether	1000000000000000000000000
+Gether	1000000000000000000000000000
+Tether	1000000000000000000000000000000
+*/
